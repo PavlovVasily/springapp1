e061ebcaac04456c1b35fbc330b0d85f7bbb5555
VasiliyPavlov <vasylyjpavlov@mail.ru>
2021-10-13 22:04:07 +0300

    lesson 9 start

--- src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
@@ -1,23 +1,6 @@
 package ru.pavlov.springcourse;
 
 public class ClassicalMusic implements Music {
-    private ClassicalMusic(){}
-
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-
-    }
-
-    public void doMyInitMethod(){
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroyMethod(){
-        System.out.println("Doing my destruction");
-    }
-
     @Override
     public String getSong() { return "Hungarian Rhapsody"; }
-
-
 }
--- src/main/java/ru/pavlov/springcourse/TestSpring.java
+++ src/main/java/ru/pavlov/springcourse/TestSpring.java
@@ -8,32 +8,14 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        Music music = context.getBean("musicBean", Music.class);
 
+        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        System.out.println(classicalMusic.getSong());
-        //System.out.println(classicalMusic2.getSong());
-
-/*        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-
-        System.out.println(firstMusicPlayer == secondMusicPlayer);
-
-
-
-        firstMusicPlayer.playMusic();
-        System.out.println(firstMusicPlayer.getName());
-
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());*/
-
-
-
+        musicPlayer.playMusic();
 
         context.close();
+
 
     }
 }
