a05f5a6c711cbc59c94743c83573d809ba86892e
VasiliyPavlov <vasylyjpavlov@mail.ru>
2021-10-14 00:19:31 +0300

    lesson10_end

--- src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
@@ -4,19 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClassicalMusic implements Music {
-    private ClassicalMusic(){}
-
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-    }
-
-    public void doMyInitMethod(){
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroyMethod(){
-        System.out.println("Doing my destruction");
-    }
 
     @Override
     public String getSong() { return "Hungarian Rhapsody"; }
--- src/main/java/ru/pavlov/springcourse/MusicPlayer.java
+++ src/main/java/ru/pavlov/springcourse/MusicPlayer.java
@@ -1,21 +1,24 @@
 package ru.pavlov.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
-    //private String name;
-    //private int volume;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-
-    //IoC
-    public MusicPlayer(Music music){
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic(){
+        return "Playing: " + classicalMusic.getSong();
     }
 
 
