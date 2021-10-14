ad18c2a3f075d82679d739a32504f6b076bfad67
PavlovVasiliy <vasylyjpavlov@mail.ru>
2021-10-14 20:39:23 +0300

    lesson11_beginHomework

--- src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/pavlov/springcourse/ClassicalMusic.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClassicalMusic implements Music {
-
     @Override
     public String getSong() { return "Hungarian Rhapsody"; }
-
-
 }
